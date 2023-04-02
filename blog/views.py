51b004a6828b8c531bc5fd2839e2c3b5b045802b
mrajani99 <54451892+mj05516@users.noreply.github.com>
2023-04-02 07:17:31 +0500

    login function

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
 
 def home(request):
     return render(request, 'blog/index.html')
+
+def loginPage(request):
+    logout(request)
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Username or password is incorrect', extra_tags='fas fa-times-circle text-white fs-3')
+
+        except:
+            messages.info(request, 'Username does not exist', extra_tags='fas fa-info-circle text-white fs-3')
+            
+    context = {}
+    return render(request, 'blog/login.html', context)
