953c9a8c6765df5d20055c1e3ebc512dae2410fb
Hussain Abbas <59961156+hussain-abbas-06228@users.noreply.github.com>
2023-04-02 10:22:36 +0500

    did a lot of things

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('login/', loginPage, name='login'),
     path('logout/', logoutUser, name='logout'),
     path('getLatestPosts/', getLatestPosts, name='getLatestPosts'),
+    # add_blogs
+    path('add_blogs/', add_blog, name='add_blog'),
+    # see_blogs with a parameter
+    path('see_blogs/<int:id>', see_blog, name='see_blog'),
+    # edit_blog
+    path('edit_blog/<int:id>', edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -47,3 +47,59 @@ def getLatestPosts(request):
     # return as ajax response
     return JsonResponse({'posts': list(posts.values())})
     
+def add_blog(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('description')
+        picture = request.FILES.get('image', '')
+        # save to database
+        post = Post.objects.create(
+            title=title,
+            content=content,
+            # save image in media folder
+            image=picture,
+
+            slug=title.replace(' ', '-'),
+            tags=title
+        )
+
+        # save image in media folder
+        post.image = picture
+
+
+
+        post.save()
+        return redirect('home')
+    else:
+    # go to add_blogs page
+        return render(request, 'blog/add_blogs.html')
+    
+
+# see_blog with parameter blog id
+def see_blog(request, id):
+    # get blog by id
+    blog = Post.objects.get(id=id)
+    context = {'blog': blog}
+    return render(request, 'blog/see_blogs.html', context)
+
+
+# edit_blog with id
+def edit_blog(request, id):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('description')
+        picture = request.FILES.get('image', '')
+        # save to database
+        post = Post.objects.get(id=id)
+        post.title = title
+        post.content = content
+        post.image = picture
+        post.slug = title.replace(' ', '-')
+        post.tags = title
+        post.save()
+        return redirect('home')
+    else:
+        # get blog by id
+        blog = Post.objects.get(id=id)
+        context = {'blog': blog}
+        return render(request, 'blog/edit_blog.html', context)
